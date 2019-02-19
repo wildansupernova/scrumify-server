45ca4b5c2522a59d0a974d264650b75390f40530
W <W>
2019-02-19 23:59:53 +0700

    adding laravel passport

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $users = factory(App\User::class, 6)->create();
     }
 }
